bed682e496c90fdfe710662250bbe66b8dd2745b
Jessie <lonsdale.jess@gmail.com>
2019-10-02 12:48:41 +0100

    get name display working now

--- app.rb
+++ app.rb
@@ -1,8 +1,17 @@
 require 'sinatra/base'
 
+
 class Battle < Sinatra::Base
+
   get '/' do
-    'testing'
+    erb :index
+  end
+
+  get '/names' do
+    p params
+    @player1 = params[:Player1]
+    @player2 = params[:Player2]
+    erb :names
   end
 
   run! if app_file == $0
--- spec/features/display_name_spec.rb
+++ spec/features/display_name_spec.rb
@@ -1,8 +1,8 @@
 feature 'display names' do
   scenario 'can display the players names' do
     visit ('/')
-    fill_in('Player 1', with: 'Jess')
-    fill_in('Player 2', with: 'Heli')
+    fill_in('Player1', with: 'Jess')
+    fill_in('Player2', with: 'Heli')
     click_button('Submit')
     expect(page).to have_content('Jess v Heli')
   end
